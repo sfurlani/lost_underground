ac687de9426db6244622c8aec02c9e551d7fa9f0
Stephen Furlani <stephen.furlani@gmail.com>
2018-01-27 13:52:36 -0500

    stuff

---
+++ Assets/_Scripts/GameEngine.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEngine : MonoBehaviour {
+
+	private int	Stations = 5;
+	private int Tunnels = 4;
+
+	public GameObject stationPrefab;
+	public GameObject tunnelPrefab;
+
+	public GameObject currentLocation; // tunnel or station
+
+	public GameObject[] allStations;
+	public GameObject[] allTunnels;
+
+	// Use this for initialization
+	void Start () {
+		
+		allStations = new GameObject[Stations];
+		for (var i = 0; i < Stations; i++) {
+			var station = Instantiate<GameObject>(stationPrefab, transform);
+			var data = station.GetComponent<StationDataModel>();
+			data.bench = GameData.benches[i];
+			data.lurch = GameData.lurches[i];
+			data.suffix = GameData.suffixes[i];
+			data.audio = GameData.audio[i];
+		}
+
+		allTunnels = new GameObject[Tunnels];
+		for (var i = 0; i < Tunnels; i++) {
+			
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+struct GameData {
+	public static readonly bool[] benches = {true, false, true, false, true};
+	public static readonly bool[] lurches = {false, true, true, false, true};
+	public static readonly string[] suffixes = {"town", "ville", "ville", "city", "town"};
+	public static readonly string[] audio = {"music", "traffic", "music", "water", "quiet"};
+}
---
+++ Assets/_Scripts/StationDataModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationDataModel : MonoBehaviour {
+
+	public bool bench;
+	public bool benchDiscovered;
+
+	public bool lurch;
+	public bool lurchDiscovered;
+
+	public string audio;
+	public bool audioDiscovered;
+
+	public string suffix;
+	public bool suffixDiscovered;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
