40f16c96bf3daaa041426af0b2c51ae9637f9425
Stephen Furlani <stephen.furlani@gmail.com>
2018-01-26 22:51:49 -0500

    main menu

---
+++ Assets/_Scripts/WakeUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WakeUp : MonoBehaviour {
+
+	Animator animator;
+	Button button;
+
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponent<Animator>();
+		button = gameObject.GetComponent<Button>();
+		button.onClick.AddListener(Fade);
+		Debug.Log("I was made");
+	}
+
+	void Fade () {
+		animator.SetTrigger("Fade");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
