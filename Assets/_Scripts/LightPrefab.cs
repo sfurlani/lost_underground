950f2986f1c5e8cd921a79a9b2c203c3fd58248b
Stephen Furlani <stephen.furlani@gmail.com>
2018-01-27 22:38:36 -0500

    animating in and out

---
+++ Assets/_Scripts/LightPrefab.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPrefab : MonoBehaviour {
+
+	Rigidbody2D body;
+	public Vector2 speed;
+
+	// Use this for initialization
+	void Start () {
+		body = gameObject.GetComponent<Rigidbody2D>();
+		body.velocity = speed;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log("wut");
+		Destroy(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_Scripts/LightScroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScroll : MonoBehaviour {
+
+	public float scrollSpeed = 0.5F;
+	public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float offset = Time.time * scrollSpeed * -1.0f;
+		rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+	}
+}
