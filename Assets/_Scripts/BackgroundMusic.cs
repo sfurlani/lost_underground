cdd5e53a39d90f4992edb5eec6067cedae9cf29b
Stephen Furlani <stephen.furlani@gmail.com>
2018-01-28 12:24:44 -0500

    forgot audio FILES

---
+++ Assets/_Scripts/BackgroundMusic.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public enum Background {
+	Trains,
+	Busker,
+	Drip,
+	Traffic,
+	None
+}
+
+public class BackgroundMusic : MonoBehaviour {
+
+	private AudioSource source;
+
+	public AudioClip tracks;
+	public AudioClip busker;
+	public AudioClip drips;
+	public AudioClip traffic;
+
+	// Use this for initialization
+	void Start () {
+		source = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void playBackground(Background bg) {
+		if (source.isPlaying) {
+			source.Stop();
+		}
+		switch (bg) {
+		case Background.Trains:
+			source.clip = tracks;
+			break;
+		case Background.Traffic:
+			source.clip = traffic;
+			break;
+		case Background.Drip:
+			source.clip = drips;
+			break;
+		case Background.Busker:
+			source.clip = busker;
+			break;
+		default:
+			// don't play anything
+			return;
+		}
+		source.Play();
+	}
+}
---
+++ Assets/_Scripts/SoundEffects.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public enum SoundEffect {
+	DoorOpen,
+	DoorClose,
+	PhoneRing,
+	Click
+}
+
+public class SoundEffects : MonoBehaviour {
+
+	public AudioSource source;
+
+	public AudioClip open;
+	public AudioClip close;
+	public AudioClip ring;
+	public AudioClip click;
+
+	// Use this for initialization
+	void Start () {
+		source = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void playSFX(SoundEffect effect) {
+		switch ( effect) {
+		case SoundEffect.Click:
+			source.clip = click;
+			break;
+		case SoundEffect.DoorOpen:
+			source.clip = open;
+			break;
+		case SoundEffect.DoorClose:
+			source.clip = close;
+			break;
+		case SoundEffect.PhoneRing:
+			source.clip = ring;
+			break;
+		default:
+			return;
+		}
+		source.Play();
+	}
+}
